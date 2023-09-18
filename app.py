ec05249d1b79c466f7818c211a617b5b1e68c10e
wallisch <walliscb@technikum-wien.at>
2023-09-18 13:44:46 +0200

    add stable-diffusion generated picture

--- app.py
+++ app.py
@@ -3,9 +3,16 @@ import os
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 import pdfparser
 import re
+import requests
+import io
+import base64
+from PIL import Image, PngImagePlugin
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['CACHE_FOLDER'] = 'cache'
+app.config['STABLE_DIFFUSION_URL'] = "http://clu.wallisch-it.net:7860"
+
 
 @app.route('/')
 def index():
@@ -44,6 +51,7 @@ def gallery():
 def viewer(filename):
     pos = request.args.get('pos', default=0, type=int)
 
+    ## fetch the file
     filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     if not os.path.isfile(filepath):
         return "File not found", 404
@@ -51,6 +59,7 @@ def viewer(filename):
     with open(filepath, 'r', encoding='utf-8') as file:
         content = file.read()
 
+    ## fetch the content, which is then used as prompt for the stable diffusion API
     paragraphs = re.split(r'\n\s*\n', content)
     if pos == 0:
         for i, paragraph in enumerate(paragraphs):
@@ -65,7 +74,25 @@ def viewer(filename):
         pos += 1
         content += paragraphs[pos if pos < len(paragraphs) else 0]
 
-    thumbnail = (os.path.join(app.config['UPLOAD_FOLDER'], filename.replace('.txt', '.jpg'))) if os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], filename.replace('.txt', '.jpg'))) else '/static/images/ebook.png'
+    ## generate the picture
+    payload = {
+        "prompt": "A steampunk image of: " + content,
+        "steps": 5
+    }
+    url = app.config['STABLE_DIFFUSION_URL']
+    response = requests.post(url=f'{url}/sdapi/v1/txt2img', json=payload)
+    r = response.json()
+    for i in r['images']:
+        image = Image.open(io.BytesIO(base64.b64decode(i.split(",",1)[0])))
+        png_payload = {
+            "image": "data:image/png;base64," + i
+        }
+        response2 = requests.post(url=f'{url}/sdapi/v1/png-info', json=png_payload)
+        pnginfo = PngImagePlugin.PngInfo()
+        pnginfo.add_text("parameters", response2.json().get("info"))
+        image.save(os.path.join(app.config['CACHE_FOLDER'], f'{filename}-{pos}.png'), pnginfo=pnginfo)
+
+    thumbnail = os.path.join(app.config['CACHE_FOLDER'], f'{filename}-{pos}.png')
 
     return render_template('viewer.html', filename=filename, content=content, thumbnail=thumbnail, nextpos=pos+1, prevpos=pos-1)
 
@@ -73,6 +100,10 @@ def viewer(filename):
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/cache/<filename>')
+def cache_file(filename):
+    return send_from_directory(app.config['CACHE_FOLDER'], filename)
 
 
 if __name__ == "__main__":
